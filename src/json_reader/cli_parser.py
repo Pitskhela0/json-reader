9bdbc0f3c8c1622ef6e3be923135f374f79416e1
Gakha Pitskhelauri <120589386+Pitskhela0@users.noreply.github.com>
2025-08-09 12:00:24 +0400

    chore: add docstrings to CLI parser

--- src/json_reader/cli_parser.py
+++ src/json_reader/cli_parser.py
@@ -2,9 +2,17 @@ import argparse
 
 
 class CLIParser:
+    """Command-line interface parser for application arguments."""
     @staticmethod
-    def parse_cli() -> tuple:
+    def parse_cli() -> tuple[str, str, str, str]:
+        """
+        Parse command-line arguments for the application.
+
+        Returns:
+            tuple: (student_file_path, room_file_path, output_format, output_destination)
+        """
         parser = argparse.ArgumentParser(description='parse CLI')
+
         parser.add_argument(
             "--student-file-path",
             type=str,
@@ -34,7 +42,9 @@ class CLIParser:
 
         arguments = parser.parse_args()
 
-        return (arguments.student_file_path,
+        return (
+                arguments.student_file_path,
                 arguments.room_file_path,
                 arguments.output_format,
-                arguments.output_destination)
+                arguments.output_destination
+        )
